a6099533fcaff1cc50936774c802de02f2048c87
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-28 14:21:28 +0200

    created applications

---
+++ online_library_django_template_project/library_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibraryAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'online_library_django_template_project.library_app'
