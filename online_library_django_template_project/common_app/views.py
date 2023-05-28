005d0233cbfe5924157517dd84b83f1d7f937ed9
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-28 15:52:20 +0200

    created book model&form

--- online_library_django_template_project/common_app/views.py
+++ online_library_django_template_project/common_app/views.py
@@ -6,6 +6,10 @@ from online_library_django_template_project.profile_app.models import UserProfil
 
 def home_page(request):
     profile = UserProfile.objects.first()
+    books = ""
+    if profile:
+        books = profile.books.all()
+
     form = CreateProfileForm(request.POST or None)
-    context = {'profile': profile, 'form': form}
-    return render(request=request, template_name='home-no-profile.html', context=context)
+    context = {'profile': profile, 'form': form, 'books': books}
+    return render(request=request, template_name='home-page.html', context=context)
--- online_library_django_template_project/library_app/models.py
+++ online_library_django_template_project/library_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from online_library_django_template_project.profile_app.models import UserProfile
+
 
 # Create your models here.
 
@@ -8,6 +10,7 @@ class Book(models.Model):
     description = models.TextField
     image = models.ImageField(upload_to='images/')
     type = models.CharField(max_length=30)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return f'{self.title} of type {self.type}'
